c6f9f5ad83ee720902e648bccb1749561b2b9e99
beocaca <51392704+beocaca@users.noreply.github.com>
2025-09-03 10:45:39 +0700

    Updated TaskCard.vue

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -52,9 +52,7 @@
           </div>
           <!-- Due Date -->
           <div v-if="task.dueDate" class="flex items-center space-x-1 text-gray-500">
-            <svg class="w-2.5 h-2.5" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11zM7 10h5v5H7z"/>
-            </svg>
+          
             <span>{{ formatDate(task.dueDate) }}</span>
           </div>
         </div>
